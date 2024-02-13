a2dee1b12d65a55c3526614b7245ce8bcf9fb46b
NirvanaSatori <morris24kariuki@gmail.com>
2024-02-13 14:16:58 +0300

    import main into index file

--- app-frontend/pages/index.js
+++ app-frontend/pages/index.js
@@ -1,21 +1,9 @@
-import Head from 'next/head'
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import Tile from "../components/Tile";
+import Main from '../components/Main';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Frontend Boilerplate React/NextJS</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <Hero />
-        <Tile />
-      </main>
-      <Footer />
+      <Main/>
     </div>
   )
 }
