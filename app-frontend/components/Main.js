2ac53d5e817d4d0095b6c9577992d137cd6fb882
NirvanaSatori <morris24kariuki@gmail.com>
2024-02-13 15:02:23 +0300

    added some comments

--- app-frontend/components/Main.js
+++ app-frontend/components/Main.js
@@ -5,8 +5,7 @@ import TransactionHistory from './TransactionHistory'
 function Main() {
     const [transactions, setTransactions] = useState([])
     const [accountBalance, setAccountBalance] = useState(0)
-
-
+  
     useEffect(()=>{ //fetch all transactions
         fetch("https://infra.devskills.app/api/accounting/transactions",{
             method:'GET',
@@ -27,8 +26,8 @@ function Main() {
                     'Content-Type': 'application/json'
                 }
             })
-            .then(r=>r.json())
-            .then(newBalance=>setAccountBalance(newBalance.balance))
+        .then(r=>r.json())
+        .then(newBalance=>setAccountBalance(newBalance.balance))
     }
 
     // add new transaction to existing ones
@@ -40,28 +39,26 @@ function Main() {
     <>
         <main className="mx-auto  max-w-7xl py-4 px-6 lg:py-4 lg:px-8 ">
             <div className="mx-auto max-w-3xl pb-12 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h1 className="sr-only">Page title</h1>
+                <h1 className="sr-only">Transaction Management</h1>
                 {/* Main 3 column grid */}
-                <div className="grid grid-cols-1 items-start gap-2 lg:grid-cols-3 lg:gap-2">
-    
+                <div className="grid grid-cols-1 items-start gap-2 lg:grid-cols-3 lg:gap-2">  
                 {/* Left column */}
                 <div className="grid grid-cols-1 gap-4    shadow-lg border-dotted rounded-md border border-lime-800">
                     <section aria-labelledby="section-2-title">
-                    <h2 className="sr-only" id="section-2-title">
-                        Submit new transaction
-                    </h2>
-                    <h2 className="flex justify-start text-xl font-bold text-gray-900 bg-gray-200 px-6 py-2">
-                        Submit new transaction
-                    </h2>
-                    <div className="overflow-hidden rounded-lg bg-white shadow h-96">
-                        <div className="p-4">
-                        
-                            <TransactionForm onAddTransaction = {addTransaction}  onHandleBalance={handleBalance}/>
+                        <h2 className="sr-only" id="section-2-title">
+                            Submit new transaction
+                        </h2>
+                        <h2 className="flex justify-start text-xl font-bold text-gray-900 bg-gray-200 px-6 py-2">
+                            Submit new transaction
+                        </h2>
+                        <div className="overflow-hidden rounded-lg bg-white shadow h-96">
+                            <div className="p-4">  
+                                {/* display transactions form*/}          
+                                <TransactionForm onAddTransaction = {addTransaction} onHandleBalance={handleBalance} />
+                            </div>
                         </div>
-                    </div>
                     </section>
                 </div>
-
                 
                 {/* right column */}
                 <div className="grid grid-cols-1 gap-4 lg:col-span-2    shadow-lg border-dotted rounded-md border border-lime-800">
@@ -73,8 +70,9 @@ function Main() {
                     Transaction History
                     </h2>
                     <div className="overflow-hidden rounded-lg bg-white shadow h-96 overflow-y-auto">
-                    <div className="p-4">
-                        <TransactionHistory accountBalance= {accountBalance} transactions={transactions}/>
+                        <div className="p-4">
+                            {/* display transactions history*/}
+                            <TransactionHistory accountBalance= {accountBalance} transactions={transactions}/>
                         </div>
                     </div>
                     </section>
