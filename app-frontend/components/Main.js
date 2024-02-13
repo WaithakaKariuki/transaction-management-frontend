76f5df611c79869563d9b0d1f83f38fc3af5b245
NirvanaSatori <morris24kariuki@gmail.com>
2024-02-13 14:19:17 +0300

    use the useEffect hook to fetch all transactions

--- app-frontend/components/Main.js
+++ app-frontend/components/Main.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Main() {
+    const [transactions, setTransactions] = useState([])
+
+    useEffect(()=>{ //fetch all transactions
+        fetch("https://infra.devskills.app/api/accounting/transactions",{
+            method:'GET',
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(r=>r.json())
+        .then(data=>{setTransactions(Object.values(data))})
+       
+    },[])
+
   return (
     <>
     </>
